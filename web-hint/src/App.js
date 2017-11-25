e4d36b1da10e16d92615ccac0b5ba1bf71ab601b
li0nbelenky <arie.belenky@gmail.com>
2017-11-25 20:43:30 +0200

    added router and cards

--- web-hint/src/App.js
+++ web-hint/src/App.js
@@ -2,20 +2,23 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Hint from './Hint'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
-const myArray = [{name: 'Arie'},
-                {name: 'Zigi'}];
+const myArray = [{title: 'Arie Belenky', subtitle: 'Software Developer'},
+                {title: 'Zigi Bigule', subtitle: 'Software Engineer'}];
 
 class App extends Component {
   render() {
     return (
+      <MuiThemeProvider>
       <div className="App">
       <ul>
       {myArray.map((item, index) =>
-          <li> <Hint name = {item.name}/></li>
+          <li> <Hint title = {item.title} subtitle={item.subtitle} /></li>
           )}
       </ul>
       </div>
+      </MuiThemeProvider>
     );
   }
 }
--- web-hint/src/Hint.js
+++ web-hint/src/Hint.js
@@ -1,10 +1,28 @@
 import React, { Component } from 'react';
+import RaisedButton from 'material-ui/RaisedButton';
+import FlatButton from 'material-ui/FlatButton';
+import {Card, CardActions, CardHeader, CardMedia, CardTitle, CardText} from 'material-ui/Card';
 
 class Hint extends Component {
   render() {
     return (
       <div className="Hint">
-      {this.props.name}
+          < Card>
+          <CardHeader
+              title={this.props.title}
+              subtitle={this.props.subtitle}
+            />
+            <CardText>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              Donec mattis pretium massa. Aliquam erat volutpat. Nulla facilisi.
+              Donec vulputate interdum sollicitudin. Nunc lacinia auctor quam sed pellentesque.
+              Aliquam dui mauris, mattis quis lacus id, pellentesque lobortis odio.
+            </CardText>
+            <CardActions>
+              <FlatButton label="Action1" />
+              <FlatButton label="Action2" />
+            </CardActions>
+          </Card>
       </div>
     );
   }
