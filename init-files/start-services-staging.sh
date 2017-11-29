9228ebcd9fed4805749e2ca68e9c38726a70630c
Gil Cohen <gil.cohen@Gilcos-MacBook-Pro.local>
2017-11-29 22:17:23 +0200

    update deploy scripts

--- init-files/start-services-staging.sh
+++ init-files/start-services-staging.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
+docker login -u ironsrcci -p 6f3TqGoqmj
 docker rm -f hint-web-server
 docker rm -f hint-web
 docker pull ironsource/hint-web-server:staging
 docker pull ironsource/hint-web:staging
-docker run -d -p 8000:8000 --name web-server ironsource/hint-web-server:staging
-docker run -d -p 3000:3000 --name hint-web ironsource/hint-web:staging
+docker run -d --network hintnet -p 8000:8000 --name web-server ironsource/hint-web-server:staging
+docker run -d --network hintnet -p 3000:3000 --name web-hint ironsource/web-hint:staging
