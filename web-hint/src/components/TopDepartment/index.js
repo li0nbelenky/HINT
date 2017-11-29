2a6a338600ba4dad1a7d354ab73e08b7812ef5f8
Daniel Zinger <daniel.zinger@DanielZ-mac.local>
2017-11-29 21:11:53 +0200

    Initial work on tags finished

--- web-hint/src/components/TopDepartment/index.js
+++ web-hint/src/components/TopDepartment/index.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react';
 import Chart from 'chart.js';
 import { HorizontalBar } from 'react-chartjs-2';
+import axios from 'axios';
+import _ from 'lodash';
 // console.log(Chart);
 
-const danielNeedsToGiveMeThis = {
+const danielNeedsToGiveMeThisForTopDepartments = {
   labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
   data: [65, 59, 80, 81, 56, 55, 40]
 };
 const data = {
-  labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+  labels: ['search', 'mobile', 'installCore', 'April', 'May', 'June', 'July'],
   datasets: [
     {
       label: 'My First dataset',
@@ -24,15 +26,44 @@ const data = {
 
 // console.log(chartData);
 class TopDepartment extends Component {
-  componentDidMount() {}
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: data
+    };
+  }
+  componentDidMount() {
+    setInterval(() => {
+      axios(
+        'http://localhost:8000/departments_impact'
+      ).then(departmentsData => {
+        const { labels, data } = departmentsData;
+        this.setState({
+          ...this.state,
+          data: {
+            labels: labels,
+            datasets: [
+              {
+                label: 'My First dataset',
+                backgroundColor: 'rgba(255,99,132,0.2)',
+                borderColor: 'rgba(255,99,132,1)',
+                borderWidth: 1,
+                hoverBackgroundColor: 'rgba(255,99,132,0.4)',
+                hoverBorderColor: 'rgba(255,99,132,1)',
+                data: data
+              }
+            ]
+          }
+        });
+      });
+    }, 1000);
+  }
   render() {
     return (
       <div className="Trends">
-        <h1>Trends - bar chart department</h1>
-        hihihi
         <div>
-          {/* <canvas id="myChartZigi" width="400" height="400" /> */}
-          <HorizontalBar data={data} />
+          <h1>Top departments</h1>
+          <HorizontalBar data={this.state.data} />
         </div>
       </div>
     );
