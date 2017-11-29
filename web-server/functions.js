3b98965c1d35f02e63858cdc929cee121c33f38b
danielledevich <daniel.ledevich@ironsrc.com>
2017-11-29 22:30:59 +0200

    base

--- web-server/functions.js
+++ web-server/functions.js
@@ -13,8 +13,8 @@ const getDepartmentsImpact = async ()=> {
 
         for (let hint in hints) {
             let impact = parseInt(hints[hint]['impact'])
-            console.log(hints[hint]['impact'])
-            console.log(depatrment)
+            // console.log(hints[hint]['impact'])
+            // console.log(depatrment)
             if (depatrment in results){
                 results[depatrment] += impact
             }else {
@@ -28,10 +28,11 @@ const getDepartmentsImpact = async ()=> {
 
     }
     console.log(results)
-
+    return sortResults(results)
 }
 
 const sortResults = function (results) {
+
     let data = {"labels": [], "data": []}
     let count = 5;
     let items = Object.keys(results).map(function(key) {
@@ -44,11 +45,13 @@ const sortResults = function (results) {
     });
 
     for (let i in items){
-        console.log(items[i])
+        // console.log(items[i])
         data['labels'] = data['labels'].concat(items[i][0])
         data['data'] = data['data'].concat(items[i][1])
     }
-    console.log(data)
+    // console.log(data)
+
+    return data
 
 // Create a new array with only the first 5 items
 //     console.log(items.slice(0, 5));
@@ -59,6 +62,10 @@ module.exports = {
     sortResults
 }
 
-getDepartmentsImpact()
+// getDepartmentsImpact()
 
 // sortResults({ IC: 10, Infra: 25, Mobile: 20, Video: 40, Super: 100, Aura: 1 })
+
+database.getHintsByTag('test', 'open').then(function (data) {
+    console.log(data)
+})
