9bf28b6a381d018254448a393f1917af864c0807
li0nbelenky <arie.belenky@gmail.com>
2017-11-29 18:00:54 +0200

    added layout style and grid for main page

--- web-hint/src/App.js
+++ web-hint/src/App.js
@@ -2,15 +2,30 @@ import React, { Component } from 'react';
 import './App.css';
 import Feed from './components/feed/Feed';
 import TopTables from './components/TopTables';
+import LatestHintReqsTable from './components/LatestHintReqsTable';
 import AddHint from './components/AddHint';
+import { Grid, Image, Segment} from 'semantic-ui-react'
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <Feed />
-        <TopTables />
-      </div>
+      <Grid columns='equal'>
+       <Grid.Row stretched>
+       <Grid.Column>
+       </Grid.Column>
+      <Grid.Column width={6}>
+          <Feed />
+        </Grid.Column>
+        <Grid.Column width={6}>
+        <Segment>
+          <TopTables />
+          </Segment>
+          <Segment>
+          <LatestHintReqsTable />
+          </Segment>
+        </Grid.Column>
+        </Grid.Row>
+      </Grid>
     );
   }
 }
