4f1af6ca69d37424b9f22b706ae08e22e20b4050
Daniel Zinger <daniel.zinger@DanielZ-mac.local>
2017-11-29 11:10:04 +0200

    Code cleanup

--- web-hint/src/components/feed/Feed.js
+++ web-hint/src/components/feed/Feed.js
@@ -1,33 +1,31 @@
 import React, { Component } from 'react';
 import './Feed.css';
-import Hint from '../hint/Hint'
-import FeedConsumer from './FeedConsumer'
-import { Feed as SemFeed, Icon } from 'semantic-ui-react'
+import Hint from '../hint/Hint';
+import FeedConsumer from './FeedConsumer';
+import { Feed as SemFeed, Icon } from 'semantic-ui-react';
 
 class Feed extends Component {
-    constructor() {
-        super();
-        this.state = {
-            hints: []
-        }
-    }
-    componentDidMount(){
-        setInterval(()=>{
-            FeedConsumer.getFeedItems().then((feedItems)=>{
-                this.setState({hints: feedItems.payload});
-            })
-        }, 1000);
-    }
+  constructor() {
+    super();
+    this.state = {
+      hints: []
+    };
+  }
+  componentDidMount() {
+    setInterval(() => {
+      FeedConsumer.getFeedItems().then(feedItems => {
+        this.setState({ hints: feedItems.payload });
+      });
+    }, 1000);
+  }
 
-    render() {
-        return (
-            <SemFeed>
-                 {this.state.hints.map((item, index) =>
-                     <Hint title = {item.title} subtitle={item.subtitle} />
-                 )}
-            </SemFeed>
-        );
-    }
+  render() {
+    return (
+      <SemFeed>
+        {this.state.hints.map((item, index) => <Hint {...item} />)}
+      </SemFeed>
+    );
+  }
 }
 
 export default Feed;
