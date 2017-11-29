630703c8c458781b5af0773eedae6227f45a24ae
Daniel Zinger <daniel.zinger@DanielZ-mac.local>
2017-11-29 17:19:35 +0200

    downgrade react version

--- web-hint/src/components/Trends/index.js
+++ web-hint/src/components/Trends/index.js
@@ -1,13 +1,54 @@
 import React, { Component } from 'react';
-
-class Trends extends Component {
-    render() {
-        return (
-            <div className="Trends">
-                <h1>Trends</h1>
-            </div>
-        );
+// import { Chart } from 'chart.js';
+import { Line } from 'react-chartjs';
+const data = {
+  labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+  datasets: [
+    {
+      label: 'My First dataset',
+      fill: false,
+      lineTension: 0.1,
+      backgroundColor: 'rgba(75,192,192,0.4)',
+      borderColor: 'rgba(75,192,192,1)',
+      borderCapStyle: 'butt',
+      borderDash: [],
+      borderDashOffset: 0.0,
+      borderJoinStyle: 'miter',
+      pointBorderColor: 'rgba(75,192,192,1)',
+      pointBackgroundColor: '#fff',
+      pointBorderWidth: 1,
+      pointHoverRadius: 5,
+      pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+      pointHoverBorderColor: 'rgba(220,220,220,1)',
+      pointHoverBorderWidth: 2,
+      pointRadius: 1,
+      pointHitRadius: 10,
+      data: [65, 59, 80, 81, 56, 55, 40]
     }
+  ]
+};
+const chartOptions = {
+  scales: {
+    yAxes: [
+      {
+        ticks: {
+          beginAtZero: true
+        }
+      }
+    ]
+  }
+};
+
+// console.log(chartData);
+class Trends extends Component {
+  render() {
+    return (
+      <div className="Trends">
+        <Line data={data} />
+        <h1>Trends</h1>
+      </div>
+    );
+  }
 }
 
 export default Trends;
