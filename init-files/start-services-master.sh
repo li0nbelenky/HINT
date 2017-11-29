d09ee13ac0797d6e3113a69d75dd9bd601b5cfaa
Gil Cohen <gil.cohen@Gilcos-MacBook-Pro.local>
2017-11-30 01:29:45 +0200

    update script with relevant name

--- init-files/start-services-master.sh
+++ init-files/start-services-master.sh
@@ -4,5 +4,5 @@ docker rm -f web-server
 docker rm -f web-hint
 docker pull ironsource/hint-web-server:master
 docker pull ironsource/hint-web:master
-docker run -d --network hintnet -p 8000:8000 --name web-server ironsource/hint-web-server:master
-docker run -d --network hintnet -p 3000:3000 --name web-hint ironsource/web-hint:master
+docker run -d --network hintnet -p 8000:8000 -e NODE_ENV=master --name web-server ironsource/hint-web-server:master
+docker run -d --network hintnet -p 3000:3000 -e NODE_ENV=master --name web-hint ironsource/hint-web:master
