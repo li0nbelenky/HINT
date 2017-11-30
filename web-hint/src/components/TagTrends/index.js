17937e1ec53c4f09e7eb6e058756e6538e45c615
Daniel Zinger <daniel.zinger@DanielZ-mac.local>
2017-11-30 11:48:40 +0200

    Update trends ui

--- web-hint/src/components/TagTrends/index.js
+++ web-hint/src/components/TagTrends/index.js
@@ -78,23 +78,25 @@ class TagTrends extends Component {
           [...this.state.data].sort()
         );
         const { labels, data } = tagTrends;
-        this.setState({
-          ...this.state,
-          data: {
-            labels: labels,
-            datasets: [
-              {
-                label: data.label,
-                backgroundColor: 'rgba(255,99,132,0.2)',
-                borderColor: 'rgba(255,99,132,1)',
-                borderWidth: 1,
-                hoverBackgroundColor: 'rgba(255,99,132,0.4)',
-                hoverBorderColor: 'rgba(255,99,132,1)',
-                data: data
+        changed
+          ? this.setState({
+              ...this.state,
+              data: {
+                labels: labels,
+                datasets: [
+                  {
+                    label: data.label,
+                    backgroundColor: 'rgba(255,99,132,0.2)',
+                    borderColor: 'rgba(255,99,132,1)',
+                    borderWidth: 1,
+                    hoverBackgroundColor: 'rgba(255,99,132,0.4)',
+                    hoverBorderColor: 'rgba(255,99,132,1)',
+                    data: data
+                  }
+                ]
               }
-            ]
-          }
-        });
+            })
+          : null;
       });
     }, 1000);
   }
