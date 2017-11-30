1322a2dc002b5560e403be898460777ded8e1409
li0nbelenky <arie.belenky@gmail.com>
2017-11-30 11:56:19 +0200

    added fake latest hints and ratings

--- web-hint/src/components/LatestHintReqsTable/index.js
+++ web-hint/src/components/LatestHintReqsTable/index.js
@@ -1,78 +1,70 @@
 import React, { Component } from 'react';
-import { Header, Image, Table } from 'semantic-ui-react'
+import { Header, Image, Table, Item, Card, Button } from 'semantic-ui-react'
 
 class LatestHintReqsTable extends Component {
     render() {
         return (
             <div className="LatestHintReqsTable">
                 <h1>Latest Hints pending for help</h1>
-                <Table basic='very' celled collapsing>
-                    <Table.Header>
-                        <Table.Row>
-                            <Table.HeaderCell>Employee</Table.HeaderCell>
-                            <Table.HeaderCell>Correct Guesses</Table.HeaderCell>
-                        </Table.Row>
-                    </Table.Header>
-
-                    <Table.Body>
-                        <Table.Row>
-                            <Table.Cell>
-                                <Header as='h4' image>
-                                    <Image src='/assets/images/avatar/small/lena.png' rounded size='mini' />
-                                    <Header.Content>
-                                        Lena
-                                        <Header.Subheader>Human Resources</Header.Subheader>
-                                    </Header.Content>
-                                </Header>
-                            </Table.Cell>
-                            <Table.Cell>
-                                22
-                            </Table.Cell>
-                        </Table.Row>
-                        <Table.Row>
-                            <Table.Cell>
-                                <Header as='h4' image>
-                                    <Image src='/assets/images/avatar/small/matthew.png' rounded size='mini' />
-                                    <Header.Content>
-                                        Matthew
-                                        <Header.Subheader>Fabric Design</Header.Subheader>
-                                    </Header.Content>
-                                </Header>
-                            </Table.Cell>
-                            <Table.Cell>
-                                15
-                            </Table.Cell>
-                        </Table.Row>
-                        <Table.Row>
-                            <Table.Cell>
-                                <Header as='h4' image>
-                                    <Image src='/assets/images/avatar/small/lindsay.png' rounded size='mini' />
-                                    <Header.Content>
-                                        Lindsay
-                                        <Header.Subheader>Entertainment</Header.Subheader>
-                                    </Header.Content>
-                                </Header>
-                            </Table.Cell>
-                            <Table.Cell>
-                                12
-                            </Table.Cell>
-                        </Table.Row>
-                        <Table.Row>
-                            <Table.Cell>
-                                <Header as='h4' image>
-                                    <Image src='/assets/images/avatar/small/mark.png' rounded size='mini' />
-                                    <Header.Content>
-                                        Mark
-                                        <Header.Subheader>Executive</Header.Subheader>
-                                    </Header.Content>
-                                </Header>
-                            </Table.Cell>
-                            <Table.Cell>
-                                11
-                            </Table.Cell>
-                        </Table.Row>
-                    </Table.Body>
-                </Table>
+                <Card>
+                    <Card>
+                        <Card.Content>
+                            <Card.Header>
+                                Daniel Zinger
+                            </Card.Header>
+                            <Card.Meta>
+                                AppQuarium Guru
+                            </Card.Meta>
+                            <Card.Description>
+                                Please help me take over the universe
+                            </Card.Description>
+                        </Card.Content>
+                        <Card.Content extra>
+                            <div className='ui two buttons'>
+                                <Button basic color='green'>Offer Help</Button>
+                                <Button basic color='red'>Ignore</Button>
+                            </div>
+                        </Card.Content>
+                    </Card>
+                    <Card>
+                        <Card.Content>
+                            <Card.Header>
+                                Shany Shmuely
+                            </Card.Header>
+                            <Card.Meta>
+                                Software Development Team Leader
+                            </Card.Meta>
+                            <Card.Description>
+                                Please help me start a "bring food from home" initiative in IronSource
+                            </Card.Description>
+                        </Card.Content>
+                        <Card.Content extra>
+                            <div className='ui two buttons'>
+                                <Button basic color='green'>Offer Help</Button>
+                                <Button basic color='red'>Ignore</Button>
+                            </div>
+                        </Card.Content>
+                    </Card>
+                    <Card>
+                        <Card.Content>
+                            <Card.Header>
+                                Lior Haiman
+                            </Card.Header>
+                            <Card.Meta>
+                                Project Manager
+                            </Card.Meta>
+                            <Card.Description>
+                                Please drink the tea
+                            </Card.Description>
+                        </Card.Content>
+                        <Card.Content extra>
+                            <div className='ui two buttons'>
+                                <Button basic color='green'>Offer Help</Button>
+                                <Button basic color='red'>Ignore</Button>
+                            </div>
+                        </Card.Content>
+                    </Card>
+                </Card>
             </div>
         );
     }
